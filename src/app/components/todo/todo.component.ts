f370a70a653aa224232d0e62432ffdea07811721
Daniel Hannus <daniel.hannus@ha.ax>
2023-02-15 20:07:12 +0200

    added some comments to the todo.component.ts

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from './../../modules/Todo';
+import { Todo } from './../../modules/Todo'; // Import the Todo interface
 
 @Component({
   selector: 'app-todos',
@@ -8,16 +8,17 @@ import { Todo } from './../../modules/Todo';
 })
 export class TodosComponent implements OnInit {
 
-  todos!: Todo[];
+  todos!: Todo[]; // Declare a todos array of type Todo
 
-  inputTodo:string = "";
+  inputTodo:string = ""; // Declare an inputTodo string for storing user input
 
-  draggedIndex: number | null = null;
-  dropIndex: number | null = null;
+  draggedIndex: number | null = null; // Store the index of the dragged todo item
+  dropIndex: number | null = null; // Store the index of the drop target todo item
 
   constructor() { }
 
   ngOnInit(): void {
+    // Initialize the todos array with some example data
     this.todos =  [
       {
         content: 'First todo',
@@ -31,42 +32,50 @@ export class TodosComponent implements OnInit {
   }
 
   toggleDone (id:number) {
+    // Update the completed state of the todo item with the given id
     this.todos.map((v,i) => {
       if (i==id) v.completed = !v.completed;
 
       return v;
     })
   }
+
   deleteTodo (id:number) {
+    // Remove the todo item with the given id from the todos array
     this.todos = this.todos.filter((v ,i) => i !== id);
   } 
 
   addTodo () {
+    // Add a new todo item to the end of the todos array
     this.todos.push({
       content:this.inputTodo,
       completed: false
     })
-    this.inputTodo="";
+    this.inputTodo=""; // Clear the inputTodo string after adding the new todo
   }
+
   onDragStart(event: DragEvent, index: number) {
+    // Store the index of the dragged todo item
     this.draggedIndex = index;
   }
 
   onDragOver(event: DragEvent, index: number) {
     event.preventDefault();
+    // Store the index of the drop target todo item
     this.dropIndex = index;
   }
 
   onDrop(event: DragEvent) {
     event.preventDefault();
     if (this.draggedIndex !== null && this.dropIndex !== null) {
+      // Create a copy of the todos array and move the dragged todo to the drop target index
       const todosCopy = [...this.todos];
       const draggedTodo = todosCopy[this.draggedIndex];
       todosCopy.splice(this.draggedIndex, 1);
       todosCopy.splice(this.dropIndex, 0, draggedTodo);
       this.todos = todosCopy;
-      this.draggedIndex = null;
-      this.dropIndex = null;
+      this.draggedIndex = null; // Reset the draggedIndex after the drop
+      this.dropIndex = null; // Reset the dropIndex after the drop
     }
   }
 }
